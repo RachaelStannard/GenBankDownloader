142f2a8bea3cbe567f9356a6d1cb9c046f5eb475
Rachael <stannarl196@potsdam.edu>
2017-04-11 08:57:58 -0400

    Beginning programming. Added what I have so far.

---
+++ src/getGenBankFile.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# IMPORTS -----------------------------
+use LWP::Simple;
+
+# CONSTANTS ---------------------------
+my $URLBase = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
+
+# MAIN --------------------------------
+print "Welcome to GenBank downloader! Ready to download files?! ^_^\n";
+
+# 	Grab file name for accession numbers
+print "What file are we accessing for the accession numbers? ";
+#chomp(my $accNumFile = <STDIN>);
+chomp(my $accNumFile = "TestFile.csv");
+open(my $accNumReader, "<", $accNumFile) or die "Could not open that file! I failed";
+print "$accNumFile has been found.\n\n";
+
+# 	Choose starting point for accession numbers
+print "Which column does the accNums begin? ";
+#chomp(my $colStart = <STDIN>);
+chomp(my $colStart = 2);
+print "\n";
+
+# 	Create the folder to store files
+print "Now what name would you like to name the folder? ";
+#chomp(my $folderName = <STDIN>);
+chomp(my $folderName = "Test1");
+print "Creating folder $folderName...\n";
+mkdir ".\/$folderName";
+print "Success!\n\n";
+
+# 	Loop through file
+while (my $entry = <$accNumReader>) {
+	chomp($entry);
+	my @row = split("\t", $entry);
+	my $species = $row[0];
+	$species =~ s/\s/_/g;
+	$species =~ s/\//_/g;
+	print "$species\n";
+	mkdir ".\/$folderName\/$species";
+	
+	for (my $i = int($colStart)-1; $i < @row; ++$i) {
+		#print $row[$i];
+
+
+		# 6 digits [XX000000], 8 digit [XXXX00000000], 9 digit IDs [XXXX000000000]
+		my $start = length($row[$i])-10;
+		my $accNum = substr $row[$i], $start, 8;
+
+		if ($accNum !~ /^[A-Z]/) {
+			$start = length($row[$i])-14;
+			$accNum = substr $row[$i], $start, 12;
+		}
+
+
+
+		print "$accNum\n";
+		my $accNumID = find_ID($accNum);	
+		
+		my $file = ".\/$folderName\/$species\/$accNum" . ".gbk";
+		print "$file\n\n";
+
+		open(my $fh, '>', $file) or die "I failed at";
+
+		print $fh $accNumID;
+		#print $fh "I did it!\n";
+		close $fh;
+	} 
+
+}
+
+print "Exiting!!\n\n";
+
+close $accNumReader;
+
+# SUB ROUTINES ------------------------
+sub find_ID {
+	my $IDURL = $URLBase . 'esearch.fcgi?db=nuccore&term=' . $_[0];
+	$IDURL =~ s/\s+//g;
+	my $returned = get($IDURL);
+	#print $returned; #testing
+	my $pattern = qr/\<Id\>([^]]+)\<\/Id\>/xi;
+	if ($returned =~ $pattern) {
+		return $1 . "\n";
+	} else {
+		print "There was an error and no acession number was found.";
+		exit;
+	}
+}
